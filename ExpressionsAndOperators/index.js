cafd6df83ec4f8bdfb5b21bd4691c8597292a422
KysilSerhii <kysil_vg16@nuwm.edu.ua>
2023-05-03 02:21:46 +0300

    done

---
+++ ExpressionsAndOperators/index.js
@@ -0,0 +1,27 @@
+// Norma
+// #1
+// const money = prompt('Введіть сумму коштів у гаманці в грн');
+// const price = prompt('Введіть ціну однієї шоколадки');
+// const result = (money / price);
+// const rest = (money % price) 
+// console.log(`Ви можете купити ${result.toFixed(0)} шоколадки і у вас залишиться ${rest}грн решти`);
+
+
+// #2
+// const someNumber = prompt('Введіть тризначне число');
+// const firstLater = someNumber % 10;
+// const second = (someNumber / 100) + '';
+// const secondNumber = second.slice(2, 3);
+// const third = (someNumber / 1000) + '';
+// const thirdNumber = third.slice(2, 3);
+// const sum = firstLater + secondNumber + thirdNumber;
+// const answer = alert(`${sum}`);
+
+
+// Max
+// #1
+
+// const deposit = prompt('Веддіть сумму вкладу під депозит на 2 місяці');
+// const interestForYear = (deposit / 100) * 5;
+// const result = ((interestForYear / 12) * 2).toFixed(1);
+// alert(result)
